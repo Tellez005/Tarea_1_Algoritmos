92543c038272fdee5bb732458ddec05fcc7eaad7
tellez <Tellez05>
2026-08-29 07:14:06 -0600

    El pepe

--- Tarea_1.cpp
+++ Tarea_1.cpp
@@ -3,12 +3,17 @@ using namespace std;
 #include <vector>
 #include <queue>
 int function(vector<int> Lista){
-    int count {0}; 
+    int count {0};
+    queue<int> fila;  
     for(int i {0}; i < Lista.size(); i++){
-    
+    if(!fila.empty()){
+        fila.push(Lista[i]);
     }
+
     return count;
+    }
 }
+
 
 int main(){
 
