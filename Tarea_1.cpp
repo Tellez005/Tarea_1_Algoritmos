d78867374e4b0d713a780fcf7b36210e9ee870b7
tellez <Tellez05>
2026-08-29 07:13:50 -0600

    El pepe

---
+++ Tarea_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <queue>
+int function(vector<int> Lista){
+    int count {0}; 
+    for(int i {0}; i < Lista.size(); i++){
+    
+    }
+    return count;
+}
+
+int main(){
+
+    return 0;
+}
